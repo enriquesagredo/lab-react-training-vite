e865acfc7df37d449f7f7f36f40ab316d27e725b
enriquesagredo <enrique.sagredo@protonmail.com>
2023-09-23 15:39:46 +0200

    feat. iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import IdCard from "./components/card/id-card";
 import data from "./data/berlin.json"
 import Greetings from "./components/greetings/greetings"
+import Random from "./components/random/random"
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       {/* <IdCard data={data} /> */}
       <Greetings lang="de">Ludwig</Greetings>
       <Greetings lang="fr">François</Greetings>
+      <Random min={2} max={6}></Random>
+      <Random min={2} max={100}></Random>
     </div>
   );
 }
--- src/components/random/random.jsx
+++ src/components/random/random.jsx
@@ -1,3 +1,11 @@
-function Random () {
-    
+function Random (num) {
+    const numRandom = Math.floor(Math.random() * (num.max - num.min + 1))
+
+    return (
+        <div>
+           <p>Numero aleatorio: {numRandom}</p> 
+        </div>
+    )
 }
+
+export default Random
